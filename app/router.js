22a173bad471af933e88646efe9b3db1b1d51587
Gerardo Buenrostro <blogcraft@gmail.com>
2016-03-18 14:23:31 -0700

    Ember base, con autenticacion  creacion de cuentas

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('protected');
+  this.route('login');
+  this.route('register');
+  this.route('confirm');
+  this.route('confirm-success');
 });
 
 export default Router;
