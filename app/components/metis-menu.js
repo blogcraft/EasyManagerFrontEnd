68a71a83a1093b83fc1ba6ad844924a578940797
Gerardo Buenrostro <blogcraft@gmail.com>
2016-03-27 01:02:00 -0300

    Menu ahora es un componente, correccion de errores menores

---
+++ app/components/metis-menu.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    invalidateSession: function() {
+      this.sendAction("invalidateSession"); // Exposes the action
+    }
+  },
+  didInsertElement: function() {
+    Ember.$('#side-menu').metisMenu();
+  }
+});
