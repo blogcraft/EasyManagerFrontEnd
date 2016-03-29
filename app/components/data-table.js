a61957f3ff3d5d51518d49fe1842dcc359dd4fad
Gerardo Buenrostro <blogcraft@gmail.com>
2016-03-29 14:25:43 -0300

    Correccion menor

--- app/components/data-table.js
+++ app/components/data-table.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function(){
     const id = this.get('DTid');
-    $(id).DataTable({
+    Ember.$(id).DataTable({
   			responsive: true
   	});
   }
