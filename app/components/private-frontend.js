dd6033109790f0bd7a21cca7f34012bdc401ba7b
Ignacio Ortuzar <iortuzar@gmail.com>
2016-04-02 20:11:19 +0100

    primera version con web promocional

---
+++ app/components/private-frontend.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    invalidateSession: function() {
+      this.sendAction("invalidateSession"); // Exposes the action
+    }
+  },
+});
