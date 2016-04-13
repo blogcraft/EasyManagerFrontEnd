d1874842654b0a96d0a1eda90e9f4c62c8f6331f
Ignacio Ortuzar <iortuzar@gmail.com>
2016-04-12 23:19:29 -0300

    conexion profile con api - frontend

---
+++ app/models/user.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  name: DS.attr('string'),
+  lastName: DS.attr('string'),
+  profession: DS.attr('string'),
+  email: DS.attr('string')
+});
--- app/routes/users/profile.js
+++ app/routes/users/profile.js
@@ -3,5 +3,30 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
+    return this.store.find('user', this.get('session.data.authenticated.uid'));
+  },
+  actions:{
+    save(model) {
+      model.set('name', model.get('firstName'));
+      model.save().then(function(){
+          display('ok');
+      });
+    }
   }
 });
+
+Ember.onerror = function (error) {
+  display('error');
+};
+
+function display(result) {
+  var div;
+  if(result == 'error') {
+    div = $("#response .alert-danger");
+  }
+  if(result == 'ok') {
+    div = $("#response .alert-success");
+  }
+
+  div.show().delay(1500).hide(1000);
+}
